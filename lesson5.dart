45d9024d6d0b3554a64ecb65ce57a7d7050998d5
kuwado <viethoan557723@gmail.com>
2026-01-07 14:08:54 +0900

    lesson 5: list

---
+++ lesson5.dart
@@ -0,0 +1,9 @@
+void main() {
+  var numbers = [1, 2, 3, 4, 5, 6]; // list chi chua 1 loai phan tu
+  numbers.add(7);
+  print('Phan tu dau tien la: ${numbers[0]}');
+  print('Phan tu cuoi cung la: ${numbers[numbers.length - 1]}');
+
+  // var objects = [];
+  // var strings = <String>[];
+}
