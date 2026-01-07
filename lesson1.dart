c4166a0d5d483f22e6b90e2b238c6fc31ccac871
Luu Viet Hoan <kuwado@fs76eefca8.tkyc513.ap.nuro.jp>
2026-01-07 11:33:58 +0900

    les1 + les2

---
+++ lesson1.dart
@@ -0,0 +1,15 @@
+void main() {
+  String message; // 変数を宣言する (hensū o sengen suru): Khai báo biến
+  message = 'Hello world'; // 変数に値を代入する (hensū ni atai o dainyū suru): Gán gia trị cho biến
+  String newMessage = 'new'; // 定義（ていぎ / teigi）: Định nghĩa
+  // 文字列（もじれつ / mojiretsu）: Chuỗi ký tự
+
+  int a = 5; // 整数型（せいすうがた / seisuugata） — kiểu số nguyên
+  double b = 3.6;
+  num c = 100; // cả int và double
+
+  var vh = "viet hoan";
+
+  print(b);
+  print(newMessage);
+}
